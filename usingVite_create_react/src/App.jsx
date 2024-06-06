beff61d46d2884a56404a3b9d18fe84832978ac8
Anjali Gupta <152984851+anjali76Codes@users.noreply.github.com>
2024-06-06 18:35:45 +0530

    React structure

--- usingVite_create_react/src/App.jsx
+++ usingVite_create_react/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>  
     <Anjali/>   
-    <h1>My Name is Anjali Gupta</h1>
+      <h2>I am Anjali Gupta</h2>
     <h3> React Course</h3>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi blanditiis expedita voluptatem porro, neque recusandae eveniet, omnis obcaecati facere vero optio reiciendis iure laborum accusamus eaque animi! Aut, optio deserunt!</p>
     </>
